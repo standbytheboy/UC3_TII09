d671487294ee405fb2798b6b0341c32d3c53b436
standby <moraislcs05@gmail.com>
2024-11-07 22:07:50 -0300

    atualizando html do dia

--- aulas/04/atividade/script.js
+++
@@ -1,8 +0,0 @@
-let age = prompt("Digite sua idade");
-let height = prompt("Digite sua altura, ex: 1.75");
-
-let trueAge = age >= 18;
-let trueHeight = height >= 1.60;
-
-let result = trueAge && trueHeight ? "Você pode entrar na atração" : "Você não pode entrar";
-document.getElementById("resultado").innerText = result;

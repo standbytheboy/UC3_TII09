be66ceae75c9cee276af19c64fd8391ab039e209
standbytheboy <moraislcs05@gmail.com>
2024-12-25 16:52:59 -0300

    atualizando jogo "clique no quadrado"

--- projeto/click_quadrado/script.js
+++ projeto/click_quadrado/script.js
@@ -1,5 +1,6 @@
 const startButton = document.getElementById("start-button");
 const square = document.getElementById("square");
+const square2 = document.getElementById("square2");
 const scoreDisplay = document.getElementById("scoreDisplay");
 const timerDisplay = document.getElementById("timerDisplay");
 const gameArea = document.getElementById("game-area");
@@ -15,6 +16,7 @@ startButton.addEventListener("click", function startGame(){
     scoreDisplay.textContent = `Pontos: ${score}`;
     timerDisplay.textContent = `Timer: ${timeLeft}s`;
     showSquare();
+    showSquare2();
     startButton.disabled = true;
     gameInterval = setInterval(() => {
         timeLeft--;
@@ -39,11 +41,29 @@ function showSquare() {
     square.style.display = "block";
 }
 
+function showSquare2() {
+    const areaW = gameArea.clientWidth - 50; // o "-50" é chamado de offset. Sem ele, o quadrado sai do limite definido.
+    const areaH = gameArea.clientHeight - 50;
+
+    const randomY = Math.floor(Math.random() * areaH)
+    const randomZ = Math.floor(Math.random() * areaW)
+
+    square2.style.top = `${randomY}px`
+    square2.style.left = `${randomZ}px`
+    square2.style.display = "block";
+}
+
+
 square.addEventListener("click", function handleSquareClick(){
     score++;
     scoreDisplay.textContent = `Pontos: ${score}`;
     showSquare();
+    showSquare2();
 });
+
+square2.addEventListener("click", () => {
+    gameOver();
+})
 
 function gameOver() {
     clearInterval(gameInterval);
